1e80c89457cb3343c4aac4c26fe00bc46450ef3d
TavlaLasha <lashatavlalashvili@yahoo.com>
2021-11-09 16:27:43 +0400

    Started Project

---
+++ LibraryAppSolution/LibraryClientApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibraryClientApp.Startup))]
+namespace LibraryClientApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
